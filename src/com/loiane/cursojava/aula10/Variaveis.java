edd17f04611a89d71a97b7b21be69976ab085085
Reginaldo Motta <62575581+reginaldomotta@users.noreply.github.com>
2022-05-22 14:14:42 -0300

    Initial commit

---
+++ src/com/loiane/cursojava/aula10/Variaveis.java
@@ -0,0 +1,16 @@
+package com.loiane.cursojava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		int idade = 37;
+		String nome = "Reginaldo";
+		String nomeDoMeuCachorro = "Duque";
+		
+		System.out.println("Idade: " + idade);
+		System.out.println("Nome: " + nome);
+		System.out.println("Nome do meu cachorro: " + nomeDoMeuCachorro);
+	}
+
+}
